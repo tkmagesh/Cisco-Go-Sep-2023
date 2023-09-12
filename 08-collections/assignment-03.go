54edd068f24a6b60fe7085200679ecaf6ae319ee
Magesh Kuppan <tkmagesh77@gmail.com>
2023-09-12 14:41:07 +0530

    assignments 03 & 04 - solution

--- 08-collections/assignment-03.go
+++ 08-collections/assignment-03.go
@@ -1,1 +1,29 @@
 /* Write a function that returns all the prime numbers beween the given start and end */
+
+package main
+
+import "fmt"
+
+func main() {
+	primes := findPrimes(2, 100)
+	fmt.Println(primes)
+}
+
+func findPrimes(start, end int) []int {
+	result := make([]int, 0)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			result = append(result, no)
+		}
+	}
+	return result
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
